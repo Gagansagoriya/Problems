acb97624656ecde9b13be260731c2ef4c5998fcb
Gagan Sagoriya <135191304+Gagansagoriya@users.noreply.github.com>
2023-11-29 09:50:01 +0530

    problem2

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+
+// Write your code here
 const http = require('http');
 
+// Create a File System 
+const fs = require('fs');
 
-// Creating a server
-const server = http.createServer((request, response) => {
-  response.end('Response received at port 8080');
-});
+const server = http.createServer((request,response) => {
 
-server.listen(8080, () => {
-  console.log('Server is listening on port 8080');
-});
+    const dataFile = fs.readFileSync('index.html').toString();
+
+    response.end(dataFile);
+})
+
+server.listen(8080,() =>{
+    console.log('Servert is listening on 8080');
+})
 
 module.exports = server;
